73dd4312d590e8277cfcbdd0c40c0c4954fac01d
Shahid Raza <raza59261@gmail.com>
2026-02-20 23:44:50 +0530

    updated data-testid

--- src/components/ColourSelector.js
+++ src/components/ColourSelector.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import  '../styles/Child.css'
+import React from "react";
+import "../styles/Child.css";
 
 const ColourSelector = (props) => {
-  const { config, selectNextBackground} = props
-  const { background, label, classname } = config
+  const { config, selectNextBackground } = props;
+  const { background, label, classname, key } = config;
   return (
-    <button className={classname} onClick={() => selectNextBackground({background: background})}>
+    <button
+      data-testid={key}
+      className={classname}
+      onClick={() => selectNextBackground({ background: background })}
+    >
       {label}
     </button>
-  )
-}
+  );
+};
 export default ColourSelector;
--- src/components/Selection.js
+++ src/components/Selection.js
@@ -2,9 +2,13 @@ import React, { useState } from "react";
 import "../styles/Child.css";
 
 const Selection = ({ item, applyColor }) => {
-   const [style,setStyle] = useState({background:''})
+  const [style, setStyle] = useState({ background: "" });
   return (
-    <div className="fix-box subheading" style={style} onClick={() => applyColor(setStyle)}>
+    <div
+      className="fix-box subheading"
+      style={style}
+      onClick={() => applyColor(setStyle)}
+    >
       {item}
     </div>
   );
